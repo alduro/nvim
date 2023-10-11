77c8747fe40f68b5650110ff0b5e572e52186e6d
alduro <aldo.nievas@gmail.com>
2023-10-11 13:27:11 -0300

    changes

--- lua/custom/plugins/indent-blankline.lua
+++ lua/custom/plugins/indent-blankline.lua
@@ -1,6 +1,7 @@
 return {
   -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
   -- Enable `lukas-reineke/indent-blankline.nvim`
   -- See `:help indent_blankline.txt`
   opts = {
@@ -9,4 +10,7 @@ return {
     show_current_context_start = true,
     show_trailing_blankline_indent = false,
   },
+  config = function()
+    require('ibl').setup()
+  end,
 }
