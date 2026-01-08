4348efcfaaa95461d50da4be59fc93d02ac07352
Aldo Nievas <aldo.nievas@gmail.com>
2026-01-08 11:52:08 -0300

    changes

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -1,11 +1,6 @@
 return {
   -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
-  -- dependencies = {
-  --   'nvim-treesitter/nvim-treesitter-textobjects',
-  -- },
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter').install({ 'go','lua','ruby','tsx','typescript', 'javascript', 'zig' }):wait(300000) -- wait max. 5 minutes
-  end,
+  lazy = false,
+  build = ':TSUpdate'
 }
